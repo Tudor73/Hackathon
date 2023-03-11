60227b58d1269d4dc1d16f3de83e693b475760c3
Tudor73 <tudorminciunescu@gmail.com>
2023-03-11 17:05:36 +0200

    auth kinda wors

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,19 +1,8 @@
-import {Entity, Column, PrimaryColumn } from "typeorm";
 
-@Entity() 
-export class User {
-    @PrimaryColumn()
+
+export type User = { 
     id: number;
-
-    @Column()
-    githubId: string;
-
-    @Column()
     name: string;
-
-    @Column()
     email: string;
-    // avatarUrl: string;
-    // createdAt: Date;
-    // updatedAt: Date;
+    githubId: string;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,76 @@
+import "reflect-metadata";
 import express  from "express";
 import passport from "passport";
-import "reflect-metadata";
-import {DataSource} from "typeorm";
-import {User} from "./entities/User";
-// import {Strategy as GitHubStrategy} from "passport-github";
-
-
-const root = __dirname + "/..";
-
-const AppDataSource = new DataSource({
-    type: "sqlite",
-    database: `${root}/data/db.sqlite`,
-    entities: [User],
-});
-
-AppDataSource.initialize().then(() => {
-
-}).catch((err) => {
-    console.log(err);
-})
-
-
-// passport.use(new GitHubStrategy({
-//     clientID: GITHUB_CLIENT_ID,
-//     clientSecret: GITHUB_CLIENT_SECRET,
-//     callbackURL: "http://127.0.0.1:3000/auth/github/callback"
-//   },
-//   function(accessToken, refreshToken, profile, cb) {
-//     User.findOrCreate({ githubId: profile.id }, function (err, user) {
-//       return cb(err, user);
-//     });
-//   }
-// ));
+import sqlite3  from "sqlite3";
+import cors from "cors";
+const session = require("express-session")
+require('dotenv').config()
+import {Strategy as GitHubStrategy} from "passport-github";
 
 
 const app = express();
+passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+  
+  passport.deserializeUser(function(user:any, done) {
+    done(null, user);
+  });
+
+
+app.use(express.json());
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: true }
+  }));
+app.use(cors({ origin: "*" }));
+const db = new sqlite3.Database("data/db.sqlite3", (err) => {if (err) console.log(err) });
+db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, githubId TEXT)", (err) => { if (err) console.log(err) });
 app.use(passport.initialize())
 
-passport.serializeUser(function(user:any, done:any) {
-    done(null, user.id);
+
+
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    // User.findOrCreate({ githubId: profile.id }, function (err, user) {
+    //   return cb(err, user);
+    // });
+    console.log(profile)
+    const user = {name: profile.username, githubId: profile.id}
+    db.run("INSERT INTO users (name, githubId) VALUES (?, ?)", [profile.username, profile.id], (err) => { return cb(err, user) });
+  }
+));
+
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+})
+
+app.get("/auth/github", passport.authenticate("github"));
+
+app.get("/auth/github/callback", passport.authenticate("github", { failureRedirect: "/login" }), (req, res) => {
+
+    res.redirect("/");
+});
+
+
+app.get("/query", (req, res) => {
+    if (!passport.session())
+        return res.status(401).send("Unauthorized");
+    return res.send("Authorized")
 });
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
+    // console.log(__dirname)
 });
+
+
 
